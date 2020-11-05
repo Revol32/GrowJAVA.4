0f656585eb12dafa8c225d367164ae682874a938
Revol32 <72274775+Revol32@users.noreply.github.com>
2020-11-06 01:32:50 +0400

    Все

--- src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
@@ -8,14 +8,14 @@ public class SimpleMathService implements MathService {
      * Метод возвращает 0, если value1 = value2.
      * Метод возвращает -1, если value1 < value2.
      * Метод возвращает 1, если value1 > value2.
-     *
+     * <p>
      * Например для (-1, -1) метод должен вернуть 0;
      * Например для (-3, -1) метод должен вернуть -1;
      * Например для (3, 1) метод должен вернуть 1;
      */
     @Override
     public int compare(int value1, int value2) {
-        return -2;
+        return Integer.compare(value1, value2);
     }
 
     /**
@@ -24,13 +24,13 @@ public class SimpleMathService implements MathService {
      */
     @Override
     public int maxFrom(int value1, int value2) {
-        return Math.max(value1,value2);
+        return Math.max(value1, value2);
     }
 
     /**
      * Метод возвращает максимальное число из переданного массива.
      * Например для списка {-1, -3, 4, 8, 5, 22, -5} метод должен вернуть 22
-     * @return
+     * return
      */
     @Override
     public int maxFrom(int[] values) {
@@ -52,7 +52,7 @@ public class SimpleMathService implements MathService {
      */
     @Override
     public int[] getEvenDigits(int[] values) {
-        return new int[]{};
+        return Arrays.stream(values).filter(i -> i % 2 == 0).toArray();
     }
 
     /**
@@ -63,19 +63,19 @@ public class SimpleMathService implements MathService {
     @Override
     public long calcFactorial(int initialVal) {
         long result = 1;
-        for (int i = 1;i<=initialVal; i++){
-            result=result*i;
+        for (int i = 1; i <= initialVal; i++) {
+            result = result * i;
         }
         return result;
     }
 
     /**
      * Метод возвращает число, которе находится на заданной позиции (счет начинается с нуля) в ряду фибоначчи.
-     *
+     * <p>
      * Ряд фибоначчи - ряд, следующие элементы которого состоят из суммы двух предыдущих.
      * Ряд начинается 0 и 1.
      * Пример 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ...
-     *
+     * <p>
      * Для числа 9 метод должен вернуть 34
      * Для числа 0 метод должен вернуть 0
      */
@@ -103,14 +103,13 @@ public class SimpleMathService implements MathService {
     /**
      * Метод определяет, является ли заданное число простым.
      * Простое число - число, которое делится только на 1 и на само себя.
-     *
+     * <p>
      * Например для числа 22 вернется false, а для числа 23 true.
      */
     @Override
     public boolean isPrimary(int number) {
-        int i;
-        for (i=2;i<number;i++){
-            if (number%i==0) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
                 return false;
             }
         }
@@ -119,7 +118,7 @@ public class SimpleMathService implements MathService {
 
     /**
      * Метод возвращает массив, в котором элементы расположены в обратном порядке.
-     *
+     * <p>
      * Например для массива {-1, -3, 4, 8, 5, 22, -5} метод вернет {-5, 22, 5, 8, 4, -3, -1}
      */
     @Override
@@ -129,6 +128,7 @@ public class SimpleMathService implements MathService {
             values[i] = values[values.length - 1 - i];
             values[values.length - 1 - i] = temp;
         }
+
         return values;
     }
 }
